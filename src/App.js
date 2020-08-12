64864ff1ba28f04b6f1ef1844a56f5b6e4b56711
melkilic <melike180400@gmail.com>
2020-08-11 22:28:52 -0500

    movies.jsx created

---
+++ src/App.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <main className="container">
+      <h1>hi</h1>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/movies.jsx
@@ -0,0 +1,8 @@
+/** @format */
+
+import React from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {}
+
+export default Movies;
