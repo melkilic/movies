6cd8fac8a4f46643211bf8e3c9f4a99ee8387731
melkilic <melike180400@gmail.com>
2020-08-24 15:49:54 -0500

    deleting a movie functionality  working

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -8,6 +8,11 @@ class Movies extends Component {
   state = {
     movies: getMovies(),
   };
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((mov) => mov._id !== movie._id);
+    //simply pass movies because the key and the value are the same
+    this.setState({ movies });
+  };
   render() {
     return (
       <table className="table">
@@ -17,15 +22,25 @@ class Movies extends Component {
             <th>Genre</th>
             <th>Stock</th>
             <th>Rate</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
           {this.state.movies.map((movie) => (
-            <tr>
+            <tr key={movie._id}>
               <td>{movie.title}</td>
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
+              <td>
+                {" "}
+                <button
+                  className="btn btn-danger btn-sm"
+                  onClick={() => this.handleDelete(movie)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
