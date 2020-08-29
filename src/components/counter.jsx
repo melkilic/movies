7e0449c31c2ee29b55881f497e284f1af709d917
melkilic <melike180400@gmail.com>
2020-08-29 14:17:46 -0500

    handleDelete function is working

---
+++ src/components/counter.jsx
@@ -0,0 +1,43 @@
+/** @format */
+
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 1,
+  };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  render() {
+    return (
+      <div>
+        <span className={this.newMethod()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement()}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={this.props.onDelete}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+
+  newMethod() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+  formatCount() {
+    return this.state.count === 0 ? "Zero" : this.state.count;
+  }
+}
+
+export default Counter;
