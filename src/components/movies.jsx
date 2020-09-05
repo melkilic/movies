5e19c734e25fec1ce2cdfb508a279a474568087a
melkilic <melike180400@gmail.com>
2020-09-04 23:36:40 -0500

    page changes handled

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -8,7 +8,8 @@ import Pagination from "./common/pagination";
 class Movies extends Component {
   state = {
     movies: getMovies(),
-    pageSize: 10,
+    pageSize: 4,
+    currentPage: 1,
   };
   handleDelete = (movie) => {
     const movies = this.state.movies.filter((mov) => mov._id !== movie._id);
@@ -24,10 +25,11 @@ class Movies extends Component {
   };
 
   handlePageChange = (page) => {
-    console.log(page);
+    this.setState({ currentPage: page });
   };
   render() {
     const { length: count } = this.state.movies;
+    const { pageSize, currentPage } = this.state;
     if (count === 0) return <p>There are no movies in the database.</p>;
 
     return (
@@ -72,7 +74,8 @@ class Movies extends Component {
         </table>
         <Pagination
           itemsCount={count}
-          pageSize={this.state.pageSize}
+          pageSize={pageSize}
+          currentPage={currentPage}
           onPageChange={this.handlePageChange}
         />
         {/* <nav aria-label="Page navigation example">
