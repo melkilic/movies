92f386b7cff63382e3112061a6b4bf96c10a2b63
melkilic <melike180400@gmail.com>
2020-09-09 22:00:06 -0500

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,86 +1,90 @@
 /** @format */
 
 import React from "react";
-import "./App.css";
-import Movies from "./components/movies";
-// import NavBar from "./components/navbar";
 import { Route, Redirect, Switch } from "react-router-dom";
+import Movies from "./components/movies";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
+import NavBar from "./components/navbar";
+import "./App.css";
 class App extends React.Component {
-  state = {
-    counters: [
-      {
-        id: 1,
-        value: 0,
-      },
-      {
-        id: 2,
-        value: 0,
-      },
-      {
-        id: 3,
-        value: 0,
-      },
-      {
-        id: 4,
-        value: 0,
-      },
-    ],
-  };
-
-  constructor(props) {
-    super(props);
-    console.log("App- Constructor");
-    // this.state= this.props.something
-  }
-  componentDidMount() {
-    console.log("App - Mounted");
-  }
-
-  handleReset = () => {
-    const counters = this.state.counters.map((c) => {
-      c.value = 0;
-      return c;
-    });
-    this.setState(counters);
-  };
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters });
-  };
-
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
-  };
-
-  handleDecrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value--;
-    this.setState({ counters });
-  };
   render() {
-    console.log("App - Rendered");
     return (
-      <main className="container">
-        <Switch>
-          <Route path="/movies" component={Movies}></Route>
-          <Route path="/customers" component={Customers}></Route>
-          <Route path="/rentals" component={Rentals}></Route>
-          <Route path="/not-found" component={NotFound}></Route>
-          <Redirect from="/" exact to="/movies" />
-          <Redirect to="/not-found" />
-        </Switch>
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
 
 export default App;
+
+/*  state = {
+  counters: [
+    {
+      id: 1,
+      value: 0,
+    },
+    {
+      id: 2,
+      value: 0,
+    },
+    {
+      id: 3,
+      value: 0,
+    },
+    {
+      id: 4,
+      value: 0,
+    },
+  ],
+};
+
+constructor(props) {
+  super(props);
+  console.log("App- Constructor");
+  // this.state= this.props.something
+}
+componentDidMount() {
+  console.log("App - Mounted");
+}
+
+handleReset = () => {
+  const counters = this.state.counters.map((c) => {
+    c.value = 0;
+    return c;
+  });
+  this.setState(counters);
+};
+handleDelete = (counterId) => {
+  const counters = this.state.counters.filter((c) => c.id !== counterId);
+  this.setState({ counters });
+};
+
+handleIncrement = (counter) => {
+  const counters = [...this.state.counters];
+  const index = counters.indexOf(counter);
+  counters[index] = { ...counter };
+  counters[index].value++;
+  this.setState({ counters });
+};
+
+handleDecrement = (counter) => {
+  const counters = [...this.state.counters];
+  const index = counters.indexOf(counter);
+  counters[index] = { ...counter };
+  counters[index].value--;
+  this.setState({ counters });
+};
+*/
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,39 @@
 /** @format */
 
 import React, { Component } from "react";
-
+import Customers from "./customers";
+import { Link, NavLink } from "react-router-dom";
 const NavBar = ({ totalCounters }) => {
   console.log("NavBar - Rendered");
   return (
-    <nav class="navbar navbar-light bg-light">
-      <a class="navbar-brand" href="#">
-        Navbar{" "}
-        <span className="badge badge-pill badge-secondary">
-          {totalCounters}
-        </span>
-      </a>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <NavLink className="navbar-brand" to="/">
+        Vidly
+      </NavLink>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <NavLink className="nav-link" to="/movies">
+            Movies
+          </NavLink>
+          <NavLink className="nav-link" to="/customers">
+            Customers
+          </NavLink>
+          <NavLink className="nav-link" to="/rentals">
+            Rentals
+          </NavLink>
+        </div>
+      </div>
     </nav>
   );
 };
