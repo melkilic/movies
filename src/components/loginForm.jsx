cbd57d388bcc2486cccdc913371c3ffc620f9774
melkilic <melike180400@gmail.com>
2020-09-10 23:57:56 -0500

    multiple inputs handled

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -3,22 +3,53 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  state = {
+    account: {
+      username: "",
+      password: "",
+    },
+  };
   handleSubmit = (e) => {
     e.preventDefault();
+    const username = this.username.current.value;
     console.log("submitted");
   };
+  handleChange = ({ currentTarget: input }) => {
+    const account = {
+      ...this.state.account,
+    };
+    //whenever you are working with the objects dynamically, use the bracket notation instead of the dot notation
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              value={account.username}
+              autoFocus
+              onChange={this.handleChange}
+              ref={this.username}
+              id="username"
+              type="text"
+              name="username"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              id="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
